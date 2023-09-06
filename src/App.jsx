f6a38a42603ccc0eb80aa3837206de6cadd923b1
Oyawe <oyawegoodness@gmail.com>
2023-09-06 14:11:25 -0700

    Hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import "./App.css";
 import { BsMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import deved from "./image/dev-ed-wave.png";
 
 function App() {
   return (
@@ -22,6 +28,28 @@ function App() {
               </li>
             </ul>
           </nav>
+          {/* Hero section */}
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-4 text-teal-600 font-medium">
+              IBTB (Ibadan_TechBro)
+            </h2>
+            <h3 className="text-2xl py-2">Developer and designer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              Freelancer providing services for programming and design content
+              needs. Join me down below and lets get cracking!
+            </p>
+          </div>
+          <div
+            className="text-5xl flex justify-center gap-16 py-3 text-gray-600
+          "
+          >
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20">
+            <img src={deved} alt="" />
+          </div>
         </section>
       </section>
     </main>
